3de3b1c8a57d4042de8de5d720eea16c287f138b
TAKAHASHI Taro <takahashi.taro.keitai@gmail.com>
2022-03-16 14:54:53 +0900

    通知アイコンにもドロップダウンを追加

--- src/components/Header2.tsx
+++ src/components/Header2.tsx
@@ -3,8 +3,16 @@ import { useState } from 'react';
 import Logo from '../svg/FM_Mail_logo.svg';
 
 const Header2 = () => {
-  const [dropdown, setDropdown] = useState(false);
-  const toggleDropdown = () => setDropdown((t) => !t);
+  const [noticeDropdown, setNoticeDropdown] = useState(false);
+  const [userDropdown, setUserDropdown] = useState(false);
+  const toggleNoticeDropdown = () => {
+    setNoticeDropdown((t) => !t);
+    setUserDropdown(false);
+  };
+  const toggleUserDropdown = () => {
+    setNoticeDropdown(false);
+    setUserDropdown((t) => !t);
+  };
 
   return (
     <nav className="bg-white shadow dark:bg-gray-800">
@@ -60,34 +68,61 @@ const Header2 = () => {
             </div>
 
             <div className="relative mt-4 flex md:mt-0">
-              <button
-                type="button"
-                className="mx-4 hidden transform text-gray-600 transition-colors duration-200 hover:text-gray-700 focus:text-gray-700 focus:outline-none dark:text-gray-200 dark:hover:text-gray-400 dark:focus:text-gray-400 md:block"
-                aria-label="show notifications"
-              >
-                <svg
-                  className="h-6 w-6"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-              </button>
               <div className="relative inline-block">
                 <button
-                  id="dropdownButton"
-                  data-dropdown-toggle="dropdown"
+                  id="noticeDropdownButton"
+                  type="button"
+                  className="mx-4 hidden h-full transform text-gray-600 transition-colors duration-200 hover:text-gray-700 focus:text-gray-700 focus:outline-none dark:text-gray-200 dark:hover:text-gray-400 dark:focus:text-gray-400 md:block"
+                  aria-label="show notifications"
+                  onClick={toggleNoticeDropdown}
+                >
+                  <svg
+                    className="h-6 w-6"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M15 17H20L18.5951 15.5951C18.2141 15.2141 18 14.6973 18 14.1585V11C18 8.38757 16.3304 6.16509 14 5.34142V5C14 3.89543 13.1046 3 12 3C10.8954 3 10 3.89543 10 5V5.34142C7.66962 6.16509 6 8.38757 6 11V14.1585C6 14.6973 5.78595 15.2141 5.40493 15.5951L4 17H9M15 17V18C15 19.6569 13.6569 21 12 21C10.3431 21 9 19.6569 9 18V17M15 17H9"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    />
+                  </svg>
+                </button>
+                <div
+                  className={
+                    noticeDropdown
+                      ? 'absolute right-0 mt-2 w-80 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 dark:bg-gray-800'
+                      : 'absolute right-0 mt-2 hidden w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 dark:bg-gray-800'
+                  }
+                >
+                  <ul
+                    className="divide-y divide-gray-100 py-1"
+                    aria-labelledby="noticeDropdownButton"
+                  >
+                    <li>
+                      <p className="mx-2 text-sm text-gray-600 dark:text-white">
+                        【FM
+                        Mailからのお知らせ】：PROプラン１か月無料キャンペーン
+                      </p>
+                    </li>
+                    <li>
+                      <p className="mx-2 text-sm text-gray-600 dark:text-white">
+                        【FM Mailからのお知らせ】：ご登録ありあがとうございます
+                      </p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="relative inline-block">
+                <button
+                  id="userDropdownButton"
                   type="button"
                   className="flex items-center focus:outline-none"
                   aria-label="toggle profile dropdown"
-                  onClick={toggleDropdown}
+                  onClick={toggleUserDropdown}
                 >
                   <div className="h-8 w-8 overflow-hidden rounded-full border-2 border-gray-400">
                     <img
@@ -102,7 +137,7 @@ const Header2 = () => {
                 </button>
                 <div
                   className={
-                    dropdown
+                    userDropdown
                       ? 'absolute right-0 mt-2 w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 dark:bg-gray-800'
                       : 'absolute right-0 mt-2 hidden w-40 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 dark:bg-gray-800'
                   }
