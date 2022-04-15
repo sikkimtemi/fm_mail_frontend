377f59a2c4b6f9964430c9258259fb5552942ea7
TAKAHASHI Taro <takahashi.taro.keitai@gmail.com>
2022-04-15 16:20:17 +0900

    APIキーの確認を実装

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Amplify from 'aws-amplify';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import awsExports from './awsExports';
 import App from './App';
 import Doc from './routes/Doc';
@@ -16,7 +18,21 @@ import Terms from './routes/Terms';
 import Tokusyouhou from './routes/Tokusyouhou';
 import ShowAPIKey from './routes/ShowAPIKey';
 
+// Amplifyの初期設定
 Amplify.configure(awsExports);
+
+// React Queryのデフォルト設定
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      suspense: true,
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
 
 const rootElement = document.getElementById('root');
 if (!rootElement) throw new Error('Failed to find the root element');
@@ -24,19 +40,24 @@ const root = ReactDOM.createRoot(rootElement);
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="api_key" element={<ShowAPIKey />} />
-        <Route path="doc" element={<Doc />} />
-        <Route path="login" element={<Login />} />
-        <Route path="mypage" element={<MyPage />} />
-        <Route path="privacy_policy" element={<PrivacyPolicy />} />
-        <Route path="signup" element={<Signup />} />
-        <Route path="terms" element={<Terms />} />
-        <Route path="thanks" element={<Thanks />} />
-        <Route path="tokusyouhou" element={<Tokusyouhou />} />
-        <Route path="*" element={<NoMatch />} />
-      </Routes>
+      <QueryClientProvider client={queryClient}>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="api_key" element={<ShowAPIKey />} />
+          <Route path="doc" element={<Doc />} />
+          <Route path="login" element={<Login />} />
+          <Route path="mypage" element={<MyPage />} />
+          <Route path="privacy_policy" element={<PrivacyPolicy />} />
+          <Route path="signup" element={<Signup />} />
+          <Route path="terms" element={<Terms />} />
+          <Route path="thanks" element={<Thanks />} />
+          <Route path="tokusyouhou" element={<Tokusyouhou />} />
+          <Route path="*" element={<NoMatch />} />
+        </Routes>
+        {process.env.NODE_ENV === 'development' && (
+          <ReactQueryDevtools initialIsOpen={false} />
+        )}
+      </QueryClientProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
