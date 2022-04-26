b6d31a47f55bff8b89be9907e8637b09238888ed
TAKAHASHI Taro <takahashi.taro.keitai@gmail.com>
2022-04-26 17:08:57 +0900

    NetlifyのFormを利用するための設定

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -14,13 +14,13 @@ const Contact: VFC = () => (
         name="contact"
         method="POST"
         data-netlify="true"
-        action="/thanks"
       >
         <div className="sm:col-span-2">
           <label htmlFor="name" className="text-sm text-gray-800 sm:text-base">
             お名前<span className="required-dot text-red-500">*</span>
             <input
               name="name"
+              type="text"
               className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
             />
           </label>
@@ -33,6 +33,7 @@ const Contact: VFC = () => (
             会社名
             <input
               name="company"
+              type="text"
               className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
             />
           </label>
@@ -42,6 +43,7 @@ const Contact: VFC = () => (
             メールアドレス<span className="required-dot text-red-500">*</span>
             <input
               name="email"
+              type="email"
               className="w-full rounded border bg-gray-50 px-3 py-2 text-gray-800 outline-none ring-indigo-300 transition duration-100 focus:ring"
             />
           </label>
@@ -58,7 +60,7 @@ const Contact: VFC = () => (
             />
           </label>
         </div>
-
+        <input type="hidden" name="form-name" value="contact" />
         <div className="flex items-center justify-between sm:col-span-2">
           <button
             type="submit"
