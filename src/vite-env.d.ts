6295359e0ae57b03d7141f33fe1fefa37028e5bd
TAKAHASHI Taro <takahashi.taro.keitai@gmail.com>
2022-04-26 15:36:26 +0900

    Amplifyの設定を環境変数に移動

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,14 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_REGION: string;
+  readonly VITE_USER_POOL_ID: string;
+  readonly VITE_USER_POOL_WEB_CLIENT_ID: string;
+  readonly VITE_OAUTH_DOMAIN: string;
+  readonly VITE_OAUTH_REDIRECT_SIGN_IN: string;
+  readonly VITE_OAUTH_REDIRECT_SIGN_OUT: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
